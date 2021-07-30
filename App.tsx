5cf614d9266c5f8d86035b157dd45dd06555845d
Parham Heidari <heidari1377@gmail.com>
2021-07-30 14:22:52 +0430

    inti project

---
+++ App.tsx
@@ -0,0 +1,119 @@
+import 'react-native-gesture-handler';
+// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { NavigationContainer } from '@react-navigation/native';
+import * as eva from '@eva-design/eva';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  ApplicationProvider,
+  BottomNavigation,
+  BottomNavigationTab,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const Section: React.FC<{
+  title: string;
+}> = ({ children, title }) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const OrdersScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category="h1">ORDERS</Text>
+  </Layout>
+);
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}>
+    <BottomNavigationTab title="USERS" />
+    <BottomNavigationTab title="ORDERS" />
+  </BottomNavigation>
+);
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name="Users" component={() => <Text>UsersScreen</Text>} />
+    <Screen name="Orders" component={() => <Text>OrdersScreen</Text>} />
+  </Navigator>
+);
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </ApplicationProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
