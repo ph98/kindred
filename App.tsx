3682a8b3fe31129dc418f1e5610ae06800e6804e
Parham Heidari <heidari1377@gmail.com>
2021-08-02 23:50:50 +0430

    small changes to appjs

--- App.tsx
+++ App.tsx
@@ -2,70 +2,28 @@ import 'react-native-gesture-handler';
 // import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import {NavigationContainer} from '@react-navigation/native';
 import * as eva from '@eva-design/eva';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabBarProps,
+} from '@react-navigation/bottom-tabs';
 import {
   ApplicationProvider,
   BottomNavigation,
   BottomNavigationTab,
-  Layout,
   Text,
 } from '@ui-kitten/components';
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {useColorScheme} from 'react-native';
+import {BottomTabBarOptions} from '@react-navigation/bottom-tabs/lib/typescript/src/types';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+interface Props extends BottomTabBarProps<BottomTabBarOptions> {
+  state: any;
+}
 
-const OrdersScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">ORDERS</Text>
-  </Layout>
-);
-
-const BottomTabBar = ({navigation, state}) => (
+const BottomTabBar: React.FC<Props> = ({navigation, state}) => (
   <BottomNavigation
     selectedIndex={state.index}
     onSelect={index => navigation.navigate(state.routeNames[index])}>
@@ -84,36 +42,13 @@ const TabNavigator = () => (
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={isDarkMode ? eva.dark : eva.light}>
       <NavigationContainer>
         <TabNavigator />
       </NavigationContainer>
     </ApplicationProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
